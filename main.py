9a6874061208be8ae97d1ceb0663162dcd20e2e8
Keyboard Technology <Abdulrasheeds13@gmail.com>
2024-08-01 21:36:37 +0100

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpg", width=450)
+    st.image("images/photo.png", width=500)
 
 with col2:
     st.title("Abdulrasheed Shittu")
@@ -17,4 +17,9 @@ with col2:
       growth and improvement..
      """
     st.info(content)
+
+content2 = """ 
+Below you can find some of the apps I have built in python. Feel free to contact me. 
+"""
+st.write(content2)
 
