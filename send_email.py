2eb11badd305e980c9b83d62f4f342d9efd3ee34
Keyboard Technology <Abdulrasheeds13@gmail.com>
2024-08-02 14:41:49 +0100

    Send emails using the form

--- send_email.py
+++ send_email.py
@@ -1,21 +1,21 @@
 import smtplib, ssl
-host = "smtp.gmail.com"
-port = 465
 
-username = "abdulrasheeds13@gmail.com"
-password = "nzjurpkyssdqavgw"
 
-receiver = "abdulrasheeds13@gmail.com"
-context = ssl.create_default_context()
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "abdulrasheeds13@gmail.com"
+    password = "nzjurpkyssdqavgw"
+    receiver = "abdulrasheeds13@gmail.com"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-how are you today
-bye!
-"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
+
 
 
 
